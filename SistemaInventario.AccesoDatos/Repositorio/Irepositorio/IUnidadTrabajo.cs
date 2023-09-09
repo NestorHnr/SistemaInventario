142fad8957431e75331488445aa9fa565c5326b8
Nestor Heredia <nestornhr27@hotmail.com>
2023-09-08 22:39:27 -0500

    Patron Repository

---
+++ SistemaInventario.AccesoDatos/Repositorio/Irepositorio/IUnidadTrabajo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.Irepositorio
+{
+    public interface IUnidadTrabajo : IDisposable
+    {
+        IBodegaRepositorio Bodega { get; }
+
+        Task Guardar();
+    }
+}
